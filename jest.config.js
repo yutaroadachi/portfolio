ca913d29f56833de2d2ba92bb97d16bbdb80e778
yutaroadachi <ytr_adc72@yahoo.co.jp>
2021-03-24 22:37:41 +0900

    fix: test-utilsのAllTheProvidersにchakraのthemeを追加

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,21 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  // see https://github.com/zeit/next.js/issues/8663
+  /** @link https://github.com/zeit/next.js/issues/8663 */
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.jest.json',
     },
   },
-  // see https://stackoverflow.com/questions/50863312/jest-gives-cannot-find-module-when-importing-components-with-absolute-paths
+  /** @link https://stackoverflow.com/questions/50863312/jest-gives-cannot-find-module-when-importing-components-with-absolute-paths */
   moduleDirectories: ['node_modules', '<rootDir>'],
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/__tests__/data'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/src/__tests__/data',
+    '<rootDir>/src/__tests__/mocks',
+  ],
+  /** @link https://stackoverflow.com/questions/47541393/syntaxerror-invalid-or-unexpected-token-import */
+  moduleNameMapper: {
+    'typeface-montserrat': '<rootDir>/src/__tests__/mocks/style.mock.js',
+  },
 }

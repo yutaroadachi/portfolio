26c1dd2ea493ce68d509e54dcb681f6281b2af91
yutaroadachi <ytr_adc72@yahoo.co.jp>
2021-08-21 14:15:16 +0900

    test: テストファイルのトランスパイラをts-jestからbabel-jestに変更

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,19 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  /** @link https://github.com/zeit/next.js/issues/8663 */
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.jest.json',
-    },
-  },
   /** @link https://stackoverflow.com/questions/50863312/jest-gives-cannot-find-module-when-importing-components-with-absolute-paths */
   moduleDirectories: ['node_modules', '<rootDir>'],
+  /** @link https://stackoverflow.com/questions/47541393/syntaxerror-invalid-or-unexpected-token-import */
+  moduleNameMapper: {
+    'typeface-montserrat': '<rootDir>/src/__tests__/mocks/style.mock.js',
+  },
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: [
     '/node_modules/',
     '<rootDir>/src/__tests__/data',
     '<rootDir>/src/__tests__/mocks',
     '<rootDir>/src/__tests__/utils',
   ],
-  /** @link https://stackoverflow.com/questions/47541393/syntaxerror-invalid-or-unexpected-token-import */
-  moduleNameMapper: {
-    'typeface-montserrat': '<rootDir>/src/__tests__/mocks/style.mock.js',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-  testEnvironment: 'jsdom',
 }

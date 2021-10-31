1bdd98b83d383e96e0ab64e5a59452805435c0e0
yutaroadachi <ytr_adc72@yahoo.co.jp>
2021-10-31 13:02:11 +0900

    refactor: RSSフィードの取得処理をリファクタ

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,20 +1,18 @@
 import { GetStaticProps } from 'next'
 import React from 'react'
-import Parser from 'rss-parser'
 import { WorksPage, WorksPageProps } from 'src/components/page/Works'
-import { RssFeed, RssFeedItem } from 'src/types/rss'
+import { getZennFeed } from 'src/lib/rss/client'
 
 export default function _WorksPage({ posts }: WorksPageProps) {
   return <WorksPage posts={posts} />
 }
 
 export const getStaticProps: GetStaticProps<WorksPageProps> = async () => {
-  const parser: Parser<RssFeed, RssFeedItem> = new Parser()
-  const feed = await parser.parseURL('https://zenn.dev/a_da_chi/feed')
+  const feed = await getZennFeed()
 
   return {
     props: {
-      posts: feed.items,
+      posts: feed,
     },
     revalidate: 5 * 60,
   }

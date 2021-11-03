ebc833aea50a007301f10140dd893eb6c6afbb25
yutaroadachi <ytr_adc72@yahoo.co.jp>
2021-11-03 23:32:02 +0900

    refactor: BlogDetailをリファクタ

--- src/components/page/BlogDetail/BlogDetail.tsx
+++ src/components/page/BlogDetail/BlogDetail.tsx
@@ -1,4 +1,4 @@
-import { Box, chakra, Stack } from '@chakra-ui/react'
+import { Box, chakra } from '@chakra-ui/react'
 import React from 'react'
 import { formatYYYYMMDD } from 'src/lib/format/date-format'
 import { PersonalBlogDetail } from 'src/types/blog'
@@ -9,13 +9,13 @@ export type BlogDetailProps = {
 
 export const BlogDetail = ({ blog }: BlogDetailProps) => {
   return (
-    <Stack spacing={4}>
-      <Box>
-        <chakra.h2 id="blog-detail" textStyle="h2">
+    <Box>
+      <Box mb={8}>
+        <chakra.h1 id="blog-detail" textStyle="h1" color="main">
           {blog.title}
-        </chakra.h2>
-        <chakra.p fontSize="12px" color="gray.500">
-          {`Posted at ${formatYYYYMMDD(new Date(blog.createdAt))}`}
+        </chakra.h1>
+        <chakra.p fontSize="14px" color="gray.500">
+          {`${formatYYYYMMDD(new Date(blog.publishedAt))}に投稿`}
         </chakra.p>
       </Box>
       <Box
@@ -24,6 +24,6 @@ export const BlogDetail = ({ blog }: BlogDetailProps) => {
         }}
         className="blog-detail"
       />
-    </Stack>
+    </Box>
   )
 }

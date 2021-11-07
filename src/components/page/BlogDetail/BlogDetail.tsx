d0e8f310b05201b35c9ac2c13991e09513fae473
yutaroadachi <ytr_adc72@yahoo.co.jp>
2021-11-07 13:02:24 +0900

    feat: BlogDetailにBioとBlogListへの導線を追加

--- src/components/page/BlogDetail/BlogDetail.tsx
+++ src/components/page/BlogDetail/BlogDetail.tsx
@@ -1,5 +1,18 @@
-import { Box, chakra } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Button,
+  Center,
+  chakra,
+  Stack,
+} from '@chakra-ui/react'
+import NextLink from 'next/link'
 import React from 'react'
+import { Biography } from 'src/components/domain/Biography'
+import { PATH } from 'src/constants/path'
 import { formatYYYYMMDD } from 'src/lib/format/date-format'
 import { PersonalBlogDetail } from 'src/types/blog'
 
@@ -11,6 +24,21 @@ export const BlogDetail = ({ blog }: BlogDetailProps) => {
   return (
     <Box>
       <Box mb={8}>
+        <Breadcrumb
+          separator={
+            <ChevronRightIcon boxSize="14px" color="gray.500" pb="2px" />
+          }
+          fontSize="14px"
+        >
+          <BreadcrumbItem>
+            <NextLink href={PATH.blog} passHref>
+              <BreadcrumbLink color="link">ブログ一覧</BreadcrumbLink>
+            </NextLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <chakra.p>ブログ詳細</chakra.p>
+          </BreadcrumbItem>
+        </Breadcrumb>
         <chakra.h1 id="blog-detail" textStyle="h1" color="main">
           {blog.title}
         </chakra.h1>
@@ -18,12 +46,22 @@ export const BlogDetail = ({ blog }: BlogDetailProps) => {
           {`${formatYYYYMMDD(new Date(blog.publishedAt))}に投稿`}
         </chakra.p>
       </Box>
-      <Box
-        dangerouslySetInnerHTML={{
-          __html: `${blog.body}`,
-        }}
-        className="blog-detail"
-      />
+      <Stack spacing={8}>
+        <Box
+          dangerouslySetInnerHTML={{
+            __html: `${blog.body}`,
+          }}
+          className="blog-detail"
+        />
+        <Center>
+          <NextLink href={PATH.blog} passHref>
+            <Button variant="outlined" size="sm" as="a">
+              ブログ一覧へ
+            </Button>
+          </NextLink>
+        </Center>
+        <Biography />
+      </Stack>
     </Box>
   )
 }

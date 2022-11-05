2ea60f199ac59b036afd4d96b0accd55f3bf73c6
yutaroadachi <ytr_adc72@yahoo.co.jp>
2022-11-05 14:08:26 +0900

    hotfix: TwitterアカウントのURLを修正

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,7 +1,7 @@
 /** 外部リンク */
 export const URL = {
   github: 'https://github.com/yutaroadachi',
-  twitter: 'https://twitter.com/a_da_chi_',
+  twitter: 'https://twitter.com/yutaroadachi',
   contact:
     'https://docs.google.com/forms/d/e/1FAIpQLSdSMQFzMIm2oWZ9fk_r1RGIUl_gaf3r6ouHVK_Ysn2FERGxVA/viewform?usp=sf_link',
 } as const

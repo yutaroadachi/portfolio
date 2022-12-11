0460f20c2d8902b1c828ce02a87c2bc8ff46f575
yutaroadachi <ytr_adc72@yahoo.co.jp>
2022-12-11 12:25:16 +0900

    feat: playwrightを導入

---
+++ e2e/top-paeg.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/')
+  await expect(page).toHaveTitle(/トップ/)
+})
+
+test.describe('リンクセクションのリンクからページへ遷移すること', () => {
+  test('/skillsへ遷移すること', async ({ page }) => {
+    const linkToSkills = page.getByTestId('skills')
+    await expect(linkToSkills).toBeVisible()
+    await linkToSkills.click()
+    await expect(page).toHaveURL(/.*skills/)
+  })
+
+  test('/resumeへ遷移すること', async ({ page }) => {
+    const linkToResume = page.getByTestId('resume')
+    await expect(linkToResume).toBeVisible()
+    await linkToResume.click()
+    await expect(page).toHaveURL(/.*resume/)
+  })
+
+  test.skip('/blogへ遷移するかはmicroCMSのデータ転送料節約のためテストしない', () => {})
+
+  test.skip('お問い合わせへ遷移するかは外部リンクのためテストしない', () => {})
+})
---
+++ playwright.config.ts
@@ -0,0 +1,37 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test'
+
+const BASE_URL = 'http://localhost:3000'
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  fullyParallel: true,
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        ...devices['iPhone 13'],
+      },
+    },
+  ],
+  retries: process.env.CI ? 1 : 0,
+  testDir: './e2e',
+  use: {
+    baseURL: process.env.PLAYWRIGHT_BASE_URL ?? BASE_URL,
+    trace: 'on-first-retry',
+  },
+  webServer: process.env.CI
+    ? undefined
+    : {
+        command: 'yarn dev',
+        url: BASE_URL,
+      },
+  workers: process.env.CI ? 1 : undefined,
+}
+
+export default config

60fa68511d0956a8aea00978cd5a193432cb1548
yutaroadachi <ytr_adc72@yahoo.co.jp>
2022-12-25 22:10:43 +0900

    chore: chakra-uiのthemeを外す

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,7 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { ReactElement, ReactNode } from 'react'
 import 'src/styles/global.css'
-import theme from 'src/theme'
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -16,9 +14,5 @@ type AppPropsWithLayout = AppProps & {
 export default function _App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return (
-    <ChakraProvider theme={theme}>
-      {getLayout(<Component {...pageProps} />)}
-    </ChakraProvider>
-  )
+  return getLayout(<Component {...pageProps} />)
 }

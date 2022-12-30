feb1405c6a22a06454b439da660596e1035494cd
yutaroadachi <ytr_adc72@yahoo.co.jp>
2022-12-30 15:34:28 +0900

    feat: html langを設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function _Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head />
       <body>
         <Main />

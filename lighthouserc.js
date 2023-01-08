37d42114bf1b173e854b5d482a9b90a5fa8c3465
yutaroadachi <ytr_adc72@yahoo.co.jp>
2023-01-08 11:57:42 +0900

    chore: lhciでpwaはチェックしない

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,14 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 1,
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ],
+      },
     },
     upload: {
       target: 'temporary-public-storage',
